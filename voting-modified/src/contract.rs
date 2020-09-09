1e5f8a8a0158c2d62915c11fe6a0dec45193eca3
bentaro <swd.ben@gmail.com>
2020-09-09 23:45:00 +0900

    modified

--- voting-modified/src/contract.rs
+++ voting-modified/src/contract.rs
@@ -1,9 +1,9 @@
 use crate::coin_helpers::assert_sent_sufficient_coin;
 use crate::msg::{
-    CreatePollResponse, HandleMsg, InitMsg, PollResponse, QueryMsg, TokenStakeResponse,
+    HandleMsg, InitMsg, QueryMsg, TokenStakeResponse,
 };
 use crate::state::{
-    bank, bank_read, config, config_read, poll, poll_read, Poll, PollStatus, State, Voter,
+    bank, bank_read, config, config_read, PollStatus, State, Voter,
 };
 use cosmwasm_std::{
     coin, log, to_binary, Api, BankMsg, Binary, CanonicalAddr, Coin, CosmosMsg, Env, Extern,
@@ -12,7 +12,7 @@ use cosmwasm_std::{
 };
 
 pub const VOTING_TOKEN: &str = "voting_token";
-pub const DEFAULT_END_HEIGHT_BLOCKS: &u64 = &100_800_u64;
+pub const DEFAULT_END_HEIGHT_BLOCKS: &u64 = &50_000_u64;
 const MIN_STAKE_AMOUNT: u128 = 1;
 const MIN_DESC_LENGTH: usize = 3;
 const MAX_DESC_LENGTH: usize = 64;
@@ -23,18 +23,28 @@ pub fn init<S: Storage, A: Api, Q: Querier>(
     msg: InitMsg,
 ) -> InitResult {
     let state = State {
-    //test
-        //denom: msg.denom,Output
-        denom: "test".to_string(),
+        denom: msg.denom,
         owner: deps.api.canonical_address(&env.message.sender)?,
-        poll_count: 0,
-        staked_tokens: Uint128::zero(),
-    };
+        staked_tokens: 0,
+        staked_tokens_yes: 0,
+        staked_tokens_no: 0,
 
+        //added
+        status: PollStatus::InProgress,
+        yes_votes: 0,
+        no_votes: 0,
+        voters: vec![],
+        voter_info: vec![],
+        start_height: msg.start_height,
+        end_height: msg.end_height,
+        description: msg.description,
+
+    };
     config(&mut deps.storage).save(&state)?;
 
     Ok(InitResponse::default())
 }
+
 
 pub fn handle<S: Storage, A: Api, Q: Querier>(
     deps: &mut Extern<S, A, Q>,
@@ -42,33 +52,23 @@ pub fn handle<S: Storage, A: Api, Q: Querier>(
     msg: HandleMsg,
 ) -> StdResult<HandleResponse> {
     match msg {
-        HandleMsg::StakeVotingTokens {} => stake_voting_tokens(deps, env),
-        HandleMsg::WithdrawVotingTokens { amount } => withdraw_voting_tokens(deps, env, amount),
-        HandleMsg::CastVote {
-            poll_id,
+        HandleMsg::StakeAndVote {
             vote,
-            weight,
-        } => cast_vote(deps, env, poll_id, vote, weight),
-        HandleMsg::EndPoll { poll_id } => end_poll(deps, env, poll_id),
-        HandleMsg::CreatePoll {
-            quorum_percentage,
-            description,
-            start_height,
-            end_height,
-        } => create_poll(
-            deps,
-            env,
-            quorum_percentage,
-            description,
-            start_height,
-            end_height,
-        ),
+        } => stake_and_vote(deps, env, vote),
+
+        HandleMsg::WithdrawVotingTokens {} => withdraw_voting_tokens(deps, env),
+
+        HandleMsg::EndPoll {} => end_poll(deps, env),
     }
 }
 
-pub fn stake_voting_tokens<S: Storage, A: Api, Q: Querier>(
+
+// integrate stake_voting_tokens and cast_vote
+
+pub fn stake_and_vote<S: Storage, A: Api, Q: Querier>(
     deps: &mut Extern<S, A, Q>,
     env: Env,
+    vote: String,
 ) -> HandleResult {
     let sender_address_raw = deps.api.canonical_address(&env.message.sender)?;
     let key = &sender_address_raw.as_slice();
@@ -76,6 +76,15 @@ pub fn stake_voting_tokens<S: Storage, A: Api, Q: Querier>(
     let mut token_manager = bank_read(&deps.storage).may_load(key)?.unwrap_or_default();
 
     let mut state = config(&mut deps.storage).load()?;
+
+    //whether voting is active or inactive
+    if state.status != PollStatus::InProgress {
+        return Err(StdError::generic_err("Poll is not in progress"));
+    }
+    //whether user has voted or not
+    if has_voted(&sender_address_raw, &state) {
+        return Err(StdError::generic_err("User has already voted."));
+    }
 
     assert_sent_sufficient_coin(
         &env.message.sent_funds,
@@ -88,57 +97,113 @@ pub fn stake_voting_tokens<S: Storage, A: Api, Q: Querier>(
         .find(|coin| coin.denom.eq(&state.denom))
         .unwrap();
 
-    token_manager.token_balance += sent_funds.amount;
+    token_manager.token_balance += sent_funds.amount.u128() as u64;
+    token_manager.vote = vote.clone();
 
-    let staked_tokens = state.staked_tokens.u128() + sent_funds.amount.u128();
-    state.staked_tokens = Uint128::from(staked_tokens);
+    let staked_tokens = state.staked_tokens + (sent_funds.amount.u128() as u64);
+    state.staked_tokens = staked_tokens;
+
+    if vote=="yes" {
+        state.staked_tokens_yes = staked_tokens;
+        state.yes_votes += 1;
+    }else{
+        state.staked_tokens_no = staked_tokens;
+        state.no_votes += 1
+    }
+
+    //add voters and voter_info
+    state.voters.push(sender_address_raw.clone());
+    let voter_info = Voter { vote };
+    state.voter_info.push(voter_info);
+
     config(&mut deps.storage).save(&state)?;
 
     bank(&mut deps.storage).save(key, &token_manager)?;
 
-    Ok(HandleResponse::default())
+    let log = vec![
+        log("action", "vote_casted"),
+        log("voter", &env.message.sender.as_str()),
+    ];
+
+    let r = HandleResponse {
+        messages: vec![],
+        log,
+        data: None,
+    };
+    Ok(r)
+}
+
+fn has_voted(voter: &CanonicalAddr, state: &State) -> bool {
+    state.voters.iter().any(|i| i == voter)
 }
 
 // Withdraw amount if not staked. By default all funds will be withdrawn.
 pub fn withdraw_voting_tokens<S: Storage, A: Api, Q: Querier>(
     deps: &mut Extern<S, A, Q>,
     env: Env,
-    amount: Option<Uint128>,
 ) -> HandleResult {
     let sender_address_raw = deps.api.canonical_address(&env.message.sender)?;
     let contract_address_raw = deps.api.canonical_address(&env.contract.address)?;
     let key = sender_address_raw.as_slice();
 
+    let mut state = config(&mut deps.storage).load()?;
+
+    // if let Pollstatus::InProgress=state.status {
+    //     Err(StdError::generic_err(
+    //         "User is trying to withdraw tokens while voting is active.",
+    //     ))
+    // }
+
+
     if let Some(mut token_manager) = bank_read(&deps.storage).may_load(key)? {
-        let largest_staked = locked_amount(&sender_address_raw, deps);
-        let withdraw_amount = match amount {
-            Some(amount) => Some(amount.u128()),
-            None => Some(token_manager.token_balance.u128()),
+        // // let largest_staked = locked_amount(&sender_address_raw, deps);
+        // let withdraw_amount = match amount {
+        //     Some(amount) => Some(amount.u128()),
+        //     None => Some(token_manager.token_balance.u128()),
+        // }
+        // .unwrap();
+
+        let mut ratio = 0.0;
+
+        match state.status {
+            PollStatus::Yes => {
+                if token_manager.vote!="yes".to_string(){
+                    return Err(StdError::generic_err(
+                        "User is trying to withdraw tokens whereas he lost.",
+                    ));
+                }
+                ratio = state.staked_tokens as f64 / state.staked_tokens_yes as f64;
+            },
+            PollStatus::No =>{
+                if token_manager.vote=="yes".to_string(){
+                    return Err(StdError::generic_err(
+                        "User is trying to withdraw tokens whereas he lost.",
+                    ));
+                }
+                ratio = state.staked_tokens as f64 / state.staked_tokens_no as f64;
+            },
+            _ =>{
+                return Err(StdError::generic_err(
+                        "User is trying to withdraw tokens while voting is active.",
+                    ));
+                }
         }
-        .unwrap();
-        if largest_staked + withdraw_amount > token_manager.token_balance.u128() {
-            Err(StdError::generic_err(
-                "User is trying to withdraw too many tokens.",
-            ))
-        } else {
-            let balance = token_manager.token_balance.u128() - withdraw_amount;
-            token_manager.token_balance = Uint128::from(balance);
+        //cast each member for calculation withdrawn balance
+        let balance = token_manager.token_balance as f64;
+        let balance = balance * ratio;
 
-            bank(&mut deps.storage).save(key, &token_manager)?;
+        token_manager.token_balance = 0;
 
-            let mut state = config(&mut deps.storage).load()?;
-            let staked_tokens = state.staked_tokens.u128() - withdraw_amount;
-            state.staked_tokens = Uint128::from(staked_tokens);
-            config(&mut deps.storage).save(&state)?;
+        bank(&mut deps.storage).save(key, &token_manager)?;
+        config(&mut deps.storage).save(&state)?;
 
-            send_tokens(
-                &deps.api,
-                &contract_address_raw,
-                &sender_address_raw,
-                vec![coin(withdraw_amount, &state.denom)],
-                "approve",
+        send_tokens(
+            &deps.api,
+            &contract_address_raw,
+            &sender_address_raw,
+            vec![coin(balance as u128, &state.denom)],
+            "approve",
             )
-        }
     } else {
         Err(StdError::generic_err("Nothing staked"))
     }
@@ -174,59 +239,6 @@ fn validate_end_height(end_height: Option<u64>, env: Env) -> StdResult<()> {
     }
 }
 
-/// create a new poll
-pub fn create_poll<S: Storage, A: Api, Q: Querier>(
-    deps: &mut Extern<S, A, Q>,
-    env: Env,
-    quorum_percentage: Option<u8>,
-    description: String,
-    start_height: Option<u64>,
-    end_height: Option<u64>,
-) -> StdResult<HandleResponse> {
-    validate_quorum_percentage(quorum_percentage)?;
-    validate_end_height(end_height, env.clone())?;
-    validate_description(&description)?;
-
-    let mut state = config(&mut deps.storage).load()?;
-    let poll_count = state.poll_count;
-    let poll_id = poll_count + 1;
-    state.poll_count = poll_id;
-
-    let sender_address_raw = deps.api.canonical_address(&env.message.sender)?;
-    let new_poll = Poll {
-        creator: sender_address_raw,
-        status: PollStatus::InProgress,
-        quorum_percentage,
-        yes_votes: Uint128::zero(),
-        no_votes: Uint128::zero(),
-        voters: vec![],
-        voter_info: vec![],
-        end_height: end_height.unwrap_or(env.block.height + DEFAULT_END_HEIGHT_BLOCKS),
-        start_height,
-        description,
-    };
-    let key = state.poll_count.to_string();
-    poll(&mut deps.storage).save(key.as_bytes(), &new_poll)?;
-
-    config(&mut deps.storage).save(&state)?;
-
-    let r = HandleResponse {
-        messages: vec![],
-        log: vec![
-            log("action", "create_poll"),
-            log(
-                "creator",
-                deps.api.human_address(&new_poll.creator)?.as_str(),
-            ),
-            log("poll_id", &poll_id.to_string()),
-            log("quorum_percentage", quorum_percentage.unwrap_or(0)),
-            log("end_height", new_poll.end_height),
-            log("start_height", start_height.unwrap_or(0)),
-        ],
-        data: Some(to_binary(&CreatePollResponse { poll_id })?),
-    };
-    Ok(r)
-}
 
 /*
  * Ends a poll. Only the creator of a given poll can end that poll.
@@ -234,180 +246,45 @@ pub fn create_poll<S: Storage, A: Api, Q: Querier>(
 pub fn end_poll<S: Storage, A: Api, Q: Querier>(
     deps: &mut Extern<S, A, Q>,
     env: Env,
-    poll_id: u64,
 ) -> HandleResult {
-    let key = &poll_id.to_string();
-    let mut a_poll = poll(&mut deps.storage).load(key.as_bytes())?;
+    let mut state = config(&mut deps.storage).load()?;
 
     let sender_address_raw = deps.api.canonical_address(&env.message.sender)?;
-    if a_poll.creator != sender_address_raw {
+    if state.owner != sender_address_raw {
         return Err(StdError::generic_err(
             "User is not the creator of the poll.",
         ));
     }
 
-    if a_poll.status != PollStatus::InProgress {
+    if state.status != PollStatus::InProgress {
         return Err(StdError::generic_err("Poll is not in progress"));
     }
 
-    if a_poll.start_height.is_some() && a_poll.start_height.unwrap() > env.block.height {
+    if state.start_height.is_some() && state.start_height.unwrap() > env.block.height {
         return Err(StdError::generic_err("Voting period has not started."));
     }
 
-    if a_poll.end_height > env.block.height {
+    if state.end_height > env.block.height {
         return Err(StdError::generic_err("Voting period has not expired."));
     }
+    let yes = state.yes_votes;
+    let  no = state.no_votes;
 
-    let mut no = 0u128;
-    let mut yes = 0u128;
-
-    for voter in &a_poll.voter_info {
-        if voter.vote == "yes" {
-            yes += voter.weight.u128();
-        } else {
-            no += voter.weight.u128();
-        }
+    //suppose either votes are 0 or same amount of votes, voting rejected
+    if yes*no==0 || yes==no{
+        state.status = PollStatus::Rejected;
     }
-    let tallied_weight = yes + no;
-
-    let poll_status = PollStatus::Rejected;
-    let mut rejected_reason = "";
-    let mut passed = false;
-
-    if tallied_weight > 0 {
-        let state = config_read(&deps.storage).load()?;
-
-        let staked_weight = deps
-            .querier
-            .query_balance(&env.contract.address, &state.denom)
-            .unwrap()
-            .amount
-            .u128();
-
-        if staked_weight == 0 {
-            return Err(StdError::generic_err("Nothing staked"));
-        }
-
-        let quorum = ((tallied_weight / staked_weight) * 100) as u8;
-        if a_poll.quorum_percentage.is_some() && quorum < a_poll.quorum_percentage.unwrap() {
-            // Quorum: More than quorum_percentage of the total staked tokens at the end of the voting
-            // period need to have participated in the vote.
-            rejected_reason = "Quorum not reached";
-        } else if yes > tallied_weight / 2 {
-            //Threshold: More than 50% of the tokens that participated in the vote
-            // (after excluding “Abstain” votes) need to have voted in favor of the proposal (“Yes”).
-            a_poll.status = PollStatus::Passed;
-            passed = true;
-        } else {
-            rejected_reason = "Threshold not reached";
-        }
-    } else {
-        rejected_reason = "Quorum not reached";
+    //suppose yes votes are greater than no, change status to No(majority lose)
+    else if yes < no{
+        state.status = PollStatus::Yes;
+    }else{
+        state.status = PollStatus::No;
     }
-    a_poll.status = poll_status;
-    poll(&mut deps.storage).save(key.as_bytes(), &a_poll)?;
 
-    for voter in &a_poll.voters {
-        unlock_tokens(deps, voter, poll_id)?;
-    }
+    config(&mut deps.storage).save(&state)?;
 
     let log = vec![
         log("action", "end_poll"),
-        log("poll_id", &poll_id.to_string()),
-        log("rejected_reason", rejected_reason),
-        log("passed", &passed.to_string()),
-    ];
-
-    let r = HandleResponse {
-        messages: vec![],
-        log,
-        data: None,
-    };
-    Ok(r)
-}
-
-// unlock voter's tokens in a given poll
-fn unlock_tokens<S: Storage, A: Api, Q: Querier>(
-    deps: &mut Extern<S, A, Q>,
-    voter: &CanonicalAddr,
-    poll_id: u64,
-) -> HandleResult {
-    let voter_key = &voter.as_slice();
-    let mut token_manager = bank_read(&deps.storage).load(voter_key).unwrap();
-
-    // unlock entails removing the mapped poll_id, retaining the rest
-    token_manager.locked_tokens.retain(|(k, _)| k != &poll_id);
-    bank(&mut deps.storage).save(voter_key, &token_manager)?;
-    Ok(HandleResponse::default())
-}
-
-// finds the largest locked amount in participated polls.
-fn locked_amount<S: Storage, A: Api, Q: Querier>(
-    voter: &CanonicalAddr,
-    deps: &mut Extern<S, A, Q>,
-) -> u128 {
-    let voter_key = &voter.as_slice();
-    let token_manager = bank_read(&deps.storage).load(voter_key).unwrap();
-    token_manager
-        .locked_tokens
-        .iter()
-        .map(|(_, v)| v.u128())
-        .max()
-        .unwrap_or_default()
-}
-
-fn has_voted(voter: &CanonicalAddr, a_poll: &Poll) -> bool {
-    a_poll.voters.iter().any(|i| i == voter)
-}
-
-pub fn cast_vote<S: Storage, A: Api, Q: Querier>(
-    deps: &mut Extern<S, A, Q>,
-    env: Env,
-    poll_id: u64,
-    vote: String,
-    weight: Uint128,
-) -> HandleResult {
-    let sender_address_raw = deps.api.canonical_address(&env.message.sender)?;
-    let poll_key = &poll_id.to_string();
-    let state = config_read(&deps.storage).load()?;
-    if poll_id == 0 || state.poll_count > poll_id {
-        return Err(StdError::generic_err("Poll does not exist"));
-    }
-
-    let mut a_poll = poll(&mut deps.storage).load(poll_key.as_bytes())?;
-
-    if a_poll.status != PollStatus::InProgress {
-        return Err(StdError::generic_err("Poll is not in progress"));
-    }
-
-    if has_voted(&sender_address_raw, &a_poll) {
-        return Err(StdError::generic_err("User has already voted."));
-    }
-
-    let key = &sender_address_raw.as_slice();
-    let mut token_manager = bank_read(&deps.storage).may_load(key)?.unwrap_or_default();
-
-    if token_manager.token_balance < weight {
-        return Err(StdError::generic_err(
-            "User does not have enough staked tokens.",
-        ));
-    }
-    token_manager.participated_polls.push(poll_id);
-    token_manager.locked_tokens.push((poll_id, weight));
-    bank(&mut deps.storage).save(key, &token_manager)?;
-
-    a_poll.voters.push(sender_address_raw.clone());
-
-    let voter_info = Voter { vote, weight };
-
-    a_poll.voter_info.push(voter_info);
-    poll(&mut deps.storage).save(poll_key.as_bytes(), &a_poll)?;
-
-    let log = vec![
-        log("action", "vote_casted"),
-        log("poll_id", &poll_id.to_string()),
-        log("weight", &weight.to_string()),
-        log("voter", &env.message.sender.as_str()),
     ];
 
     let r = HandleResponse {
@@ -450,33 +327,7 @@ pub fn query<S: Storage, A: Api, Q: Querier>(
         QueryMsg::Config {} => to_binary(&config_read(&_deps.storage).load()?),
 
         QueryMsg::TokenStake { address } => token_balance(_deps, address),
-        QueryMsg::Poll { poll_id } => query_poll(_deps, poll_id),
     }
-}
-
-fn query_poll<S: Storage, A: Api, Q: Querier>(
-    deps: &Extern<S, A, Q>,
-    poll_id: u64,
-) -> StdResult<Binary> {
-    let key = &poll_id.to_string();
-//poll_readはstateにて定義、バイナリをオブジェクト化
-    let poll = match poll_read(&deps.storage).may_load(key.as_bytes())? {
-//型マッチしていれば返す
-        Some(poll) => Some(poll),
-        None => return Err(StdError::generic_err("Poll does not exist")),
-    }
-    .unwrap();
-//pollオブジェクトの情報とメタデータからオブジェクト生成
-    let resp = PollResponse {
-        creator: deps.api.human_address(&poll.creator).unwrap(),
-        status: poll.status,
-        quorum_percentage: poll.quorum_percentage,
-        end_height: Some(poll.end_height),
-        start_height: poll.start_height,
-        description: poll.description,
-    };
-//バイナリで返す
-    to_binary(&resp)
 }
 
 fn token_balance<S: Storage, A: Api, Q: Querier>(
--- voting-modified/src/state.rs
+++ voting-modified/src/state.rs
@@ -14,44 +14,54 @@ static BANK_KEY: &[u8] = b"bank";
 pub struct State {
     pub denom: String,
     pub owner: CanonicalAddr,
-    pub poll_count: u64,
-    pub staked_tokens: Uint128,
-}
-
-#[derive(Default, Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct TokenManager {
-    pub token_balance: Uint128,             // total staked balance
-    pub locked_tokens: Vec<(u64, Uint128)>, //maps poll_id to weight voted
-    pub participated_polls: Vec<u64>,       // poll_id
-}
-
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct Voter {
-    pub vote: String,
-    pub weight: Uint128,
-}
-
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub enum PollStatus {
-    InProgress,
-    Tally,
-    Passed,
-    Rejected,
-}
-
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct Poll {
-    pub creator: CanonicalAddr,
+    pub staked_tokens: u64,
+    pub staked_tokens_yes: u64,
+    pub staked_tokens_no: u64,
     pub status: PollStatus,
-    pub quorum_percentage: Option<u8>,
-    pub yes_votes: Uint128,
-    pub no_votes: Uint128,
+    pub yes_votes: u64,
+    pub no_votes: u64,
     pub voters: Vec<CanonicalAddr>,
     pub voter_info: Vec<Voter>,
     pub end_height: u64,
     pub start_height: Option<u64>,
     pub description: String,
+
 }
+
+#[derive(Default, Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct TokenManager {
+    pub token_balance: u64,             // total staked balance
+    pub vote: String,                       // vote info ("yes" or "no")
+    // pub locked_tokens: Vec<(u64, Uint128)>, //maps poll_id to weight voted
+    // pub participated_polls: Vec<u64>,       // poll_id
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct Voter {
+    pub vote: String,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub enum PollStatus {
+    InProgress,
+    Yes,
+    No,
+    Rejected,
+}
+
+// #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+// pub struct Poll {
+//     pub creator: CanonicalAddr,
+//     pub status: PollStatus,
+//     pub quorum_percentage: Option<u8>,
+//     pub yes_votes: Uint128,
+//     pub no_votes: Uint128,
+//     pub voters: Vec<CanonicalAddr>,
+//     pub voter_info: Vec<Voter>,
+//     pub end_height: u64,
+//     pub start_height: Option<u64>,
+//     pub description: String,
+// }
 
 pub fn config<S: Storage>(storage: &mut S) -> Singleton<S, State> {
     singleton(storage, CONFIG_KEY)
@@ -61,13 +71,13 @@ pub fn config_read<S: Storage>(storage: &S) -> ReadonlySingleton<S, State> {
     singleton_read(storage, CONFIG_KEY)
 }
 
-pub fn poll<S: Storage>(storage: &mut S) -> Bucket<S, Poll> {
-    bucket(POLL_KEY, storage)
-}
-
-pub fn poll_read<S: Storage>(storage: &S) -> ReadonlyBucket<S, Poll> {
-    bucket_read(POLL_KEY, storage)
-}
+// pub fn poll<S: Storage>(storage: &mut S) -> Bucket<S, Poll> {
+//     bucket(POLL_KEY, storage)
+// }
+//
+// pub fn poll_read<S: Storage>(storage: &S) -> ReadonlyBucket<S, Poll> {
+//     bucket_read(POLL_KEY, storage)
+// }
 
 pub fn bank<S: Storage>(storage: &mut S) -> Bucket<S, TokenManager> {
     bucket(BANK_KEY, storage)
